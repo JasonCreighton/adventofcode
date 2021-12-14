3fd90199e86323e6a4b3d598dd810e97fbde6d1b
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-13 21:06:04 -0700

    Day 13 part 1

---
+++ 2015/day13.pl
@@ -0,0 +1,46 @@
+:- dynamic happiness/3.
+
+read_file(File, Str) :-
+    setup_call_cleanup(
+        open(File, read, In),
+        read_string(In, _, Str),
+        close(In)).
+
+load_facts :-
+    retractall(happiness(_, _, _)),
+    read_file("inputs/day13.txt", Str),
+    split_string(Str, "\n", ".", Lines),
+    member(Line, Lines),
+    string_lower(Line, LowerLine),
+    split_string(LowerLine, " ", "", Words),
+    maplist(atom_string, Atoms, Words),
+    process_line(Atoms).
+
+process_line([PersonA, would, GainOrLose, NAtom, happiness, units, by, sitting, next, to, PersonB]) :-
+    atom_number(NAtom, N),
+    (
+        (GainOrLose = gain, AdjN = N)
+    ;   (GainOrLose = lose, AdjN is -N)
+    ),
+    assert(happiness(PersonA, PersonB, AdjN)).
+
+net_happiness(A, B, N) :-
+    happiness(A, B, ADelta),
+    happiness(B, A, BDelta),
+    N is ADelta + BDelta.
+
+seating(S) :-
+    permutation([alice, bob, carol, david, eric, frank, george, mallory], S).
+
+seating_happiness([A], Happiness, Z) :- net_happiness(A, Z, Happiness).
+seating_happiness([A,B|Rest], Happiness, Z) :-
+    net_happiness(A, B, ABHappiness),
+    seating_happiness([B|Rest], RestHappiness, Z),
+    Happiness is ABHappiness + RestHappiness.
+
+seating_happiness([First|Rest], Happiness) :- seating_happiness([First|Rest], Happiness, First).
+
+part1_answer(N) :-
+    aggregate_all(max(H), (seating(S), seating_happiness(S, H)), N).
+
+:- forall(load_facts, true).
