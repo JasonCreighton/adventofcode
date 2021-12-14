ca3617c964539f7bea09a29e35e1b0efdc2354d6
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-13 19:03:33 -0700

    Day 12 part 2

--- 2015/day12.pl
+++ 2015/day12.pl
@@ -6,19 +6,28 @@ puzzle_input(Json) :-
         json_read_dict(In, Json),
         close(In)).
 
-json_number_total(N, N) :- number(N).
-json_number_total(Str, 0) :- string(Str).
-json_number_total([], 0).
-json_number_total([X|XS], N) :-
-    json_number_total(X, ThisN),
-    json_number_total(XS, RestN),
+json_number_total(_, N, N) :- number(N).
+json_number_total(_, Str, 0) :- string(Str).
+json_number_total(_, [], 0).
+json_number_total(IgnoreRed, [X|XS], N) :-
+    json_number_total(IgnoreRed, X, ThisN),
+    json_number_total(IgnoreRed, XS, RestN),
     N is ThisN + RestN.
-json_number_total(Dict, N) :-
+json_number_total(IgnoreRed, Dict, N) :-
     is_dict(Dict),
     dict_pairs(Dict, _, Pairs),
     pairs_values(Pairs, Values),
-    json_number_total(Values, N).
+    (
+        (IgnoreRed, member("red", Values))
+    ->  N = 0
+    ;   json_number_total(IgnoreRed, Values, N)
+    )
+    .
 
 part1_answer(N) :-
     puzzle_input(Json),
-    json_number_total(Json, N).
+    json_number_total(false, Json, N).
+
+part2_answer(N) :-
+    puzzle_input(Json),
+    json_number_total(true, Json, N).
