e833e78b166e395cfb6183c0d7b85e0470c4f6f6
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-14 17:53:06 -0700

    Day 14 part 2

--- 2015/day14.pl
+++ 2015/day14.pl
@@ -8,6 +8,12 @@ reindeer_desc(comet, 3, 37, 76).
 reindeer_desc(prancer, 9, 12, 97).
 reindeer_desc(dancer, 37, 1, 36).
 
+% Example data
+/*
+reindeer_desc(comet, 14, 10, 127).
+reindeer_desc(dancer, 16, 11, 162).
+*/
+
 reindeer_time_distance(Reindeer, Time, Distance) :-
     reindeer_desc(Reindeer, Speed, SpeedTime, RestTime),
     Time >= 0,
@@ -17,5 +23,18 @@ reindeer_time_distance(Reindeer, Time, Distance) :-
     DistanceInCycle is Speed * min(SpeedTime, PositionInCycle),
     Distance is (Speed * SpeedTime * NumCycles) + DistanceInCycle.
 
+leading_reindeer_time(Reindeer, Time) :-
+    aggregate_all(max(D), reindeer_time_distance(_, Time, D), MaxDistance),
+    reindeer_time_distance(Reindeer, Time, MaxDistance).
+
+part2_points(Reindeer, Time, 1) :- leading_reindeer_time(Reindeer, Time).
+part2_points(Reindeer, Time, 0) :- \+ leading_reindeer_time(Reindeer, Time).
+
+part2_score(Reindeer, Time, Score) :-
+    aggregate(sum(Points), T^(between(1, Time, T), part2_points(Reindeer, T, Points)), Score).
+
 part1_answer(N) :-
     aggregate_all(max(Dist), reindeer_time_distance(_, 2503, Dist), N).
+
+part2_answer(N) :-
+    aggregate_all(max(Score), part2_score(_, 2503, Score), N).
