4762692e3bdaa7791417484397e4abf008456514
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-15 17:49:48 -0700

    Day 17 part 2

--- 2015/day17.pl
+++ 2015/day17.pl
@@ -1,13 +1,15 @@
 puzzle_input([43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34, 17, 17, 44, 36, 31, 46, 9, 27, 38]).
 
-list_total_ways([], 0, 1).
-list_total_ways([], Total, 0) :- Total \= 0.
-list_total_ways([X|XS], Total, Ways) :-
-    TotalMinusX is Total - X,
-    list_total_ways(XS, TotalMinusX, WaysWithX),
-    list_total_ways(XS, Total, WaysWithoutX),
-    Ways is WaysWithX + WaysWithoutX.
+ordered_sublist(_, []).
+ordered_sublist([X|XS], [Y|YS]) :-
+    (X = Y, ordered_sublist(XS, YS))
+;   ordered_sublist(XS, [Y|YS]).
 
 part1_answer(N) :-
     puzzle_input(Input),
-    list_total_ways(Input, 150, N).
+    aggregate_all(count, (ordered_sublist(Input, Used), sum_list(Used, 150)), N).
+
+part2_answer(N) :-
+    puzzle_input(Input),
+    aggregate_all(min(NumUsed), (ordered_sublist(Input, Used), sum_list(Used, 150), length(Used, NumUsed)), MinNumUsed),
+    aggregate_all(count, (ordered_sublist(Input, Used), length(Used, MinNumUsed), sum_list(Used, 150)), N).
