9290533845ee7ad071e16607659580e9000f55f1
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-16 19:18:03 -0700

    Day 19 initial progress (not solved yet)

---
+++ 2015/day19.pl
@@ -0,0 +1,44 @@
+:- dynamic replacement/2.
+:- dynamic puzzle_input/1.
+
+read_file(File, Str) :-
+    setup_call_cleanup(
+        open(File, read, In),
+        read_string(In, _, Str),
+        close(In)).
+
+load_facts :-
+    retractall(replacement(_, _)),
+    retractall(puzzle_input(_)),
+    read_file("inputs/day19.txt", Str),
+    split_string(Str, "\n", "", Lines),
+    member(Line, Lines),
+    process_line(Line).
+
+string_molecule_list_helper(ReCaptures, Acc, [Molecule|Acc]) :-
+    atom_string(Molecule, ReCaptures.0).
+
+string_molecule_list(Str, MoleculeList) :-
+    re_foldl(string_molecule_list_helper, "[A-Z][a-z]*", Str, [], RevMoleculeList, []),
+    reverse(RevMoleculeList, MoleculeList).
+
+process_line(Line) :-
+    re_matchsub("^([A-Za-z]+) => ([A-Za-z]+)$", Line, Captures, []),
+    !,
+    atom_string(From, Captures.1),
+    string_molecule_list(Captures.2, To),
+    assert(replacement(From, To)).
+
+process_line(Line) :-
+    re_match("^[A-Za-z]+$", Line),
+    !,
+    string_molecule_list(Line, PuzInput),
+    assert(puzzle_input(PuzInput)).
+
+possible_replacement([], []).
+possible_replacement([X|XS], Replacement) :-
+    (X2 = [X]; replacement(X, X2)),
+    possible_replacement(XS, XS2),
+    append(X2, XS2, Replacement).
+
+:- forall(load_facts, true).
