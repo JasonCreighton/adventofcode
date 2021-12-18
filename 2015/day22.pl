2114b04046f6321a545fa02e8d257571862b7b8d
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-18 11:33:28 -0700

    Day 22 WIP
    This might be sort of correct, but it doesn't deal with the exponential blowup, I need to prune the search somehow.

--- 2015/day22.pl
+++ 2015/day22.pl
@@ -23,3 +23,35 @@ effect_active(Spell, SpellHistory) :-
 
 active_effects_sum(SpellHistory, Armor, Damage, Mana) :-
     aggregate_all(x(sum(A), sum(D), sum(M)), (effect(E, _, A, D, M), effect_active(E, SpellHistory)), x(Armor, Damage, Mana)).
+
+turn(gamestate(player, PlayerHitPoints, PlayerMana, BossHitPoints, SpellHistory),
+     gamestate(boss, NewPlayerHitPoints, NewPlayerMana, NewBossHitPoints, [Spell|SpellHistory])) :-
+    spell(Spell, SpellManaCost, SpellDamage, SpellHealing),
+    active_effects_sum(SpellHistory, _, EffectDamage, EffectMana),
+    NewPlayerHitPoints is PlayerHitPoints + SpellHealing,
+    NewPlayerMana is PlayerMana - SpellManaCost + EffectMana,
+    NewPlayerMana >= 0,
+    NewBossHitPoints is BossHitPoints - SpellDamage - EffectDamage.
+
+turn(gamestate(boss, PlayerHitPoints, PlayerMana, BossHitPoints, SpellHistory),
+     gamestate(player, NewPlayerHitPoints, NewPlayerMana, NewBossHitPoints, [bossattack|SpellHistory])) :-
+    boss_stats(_, BossDamage),
+    active_effects_sum(SpellHistory, EffectArmor, EffectDamage, EffectMana),
+    NewPlayerHitPoints is PlayerHitPoints - max(1, BossDamage - EffectArmor),
+    NewPlayerMana is PlayerMana + EffectMana,
+    NewBossHitPoints is BossHitPoints - EffectDamage.
+
+in_progress(gamestate(_, PHP, _, BHP, _)) :- PHP >= 0, BHP >= 0.
+
+winner(player, gamestate(_, _, _, BHP, _)) :- BHP < 0.
+winner(boss, gamestate(_, PHP, _, BHP, _)) :- BHP >= 0, PHP < 0.
+
+eventual_winner(Winner, State, FinalState) :-
+    (
+        in_progress(State)
+    ->  (
+            turn(State, NextState),
+            eventual_winner(Winner, NextState, FinalState)
+        )
+    ;   winner(Winner, State)
+    ).
