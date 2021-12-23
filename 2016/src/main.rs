a53d9e57dff27b42830ca8a02b2ec91549b95ef1
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-23 08:56:49 -0700

    Make day selectable on command line, mostly to avoid unused code warnings

--- 2016/src/main.rs
+++ 2016/src/main.rs
@@ -2,6 +2,13 @@ mod day1;
 mod day2;
 
 fn main() {
-    // day1::run();
-    day2::run();
+    let args: Vec<String> = std::env::args().collect();
+    let latest_day = 2;
+    let day : i32 = args[1].parse().unwrap_or(latest_day);
+
+    match day {
+        1 => day1::run(),
+        2 => day2::run(),
+        _ => println!("Day {} not implemented.", day),
+    }
 }
