0adac027c4ca59bb700dffcc889c47ac80e2b132
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-23 10:26:24 -0700

    2016 Day 3: My earlier goof was reporting the number of impossible triangles, not the number of possible triangles.

--- 2016/src/day3.rs
+++ 2016/src/day3.rs
@@ -1,17 +1,31 @@
-fn part1(puzzle_input: &str) -> i32 {
-    let mut num_impossible = 0;
-    for line in puzzle_input.lines() {
-        let mut triangle = line.split_whitespace().map(|s| s.parse().unwrap()).collect::<Vec<i32>>();
-        triangle.sort();
-        if (triangle[0] + triangle[1]) <= triangle[2] {
-            println!("Impossible: {:?}", triangle);
-            num_impossible += 1;
+fn count_possible(triangles: &Vec<Vec<i32>>) -> i32 {
+    let mut num_possible = 0;
+    for triangle in triangles {
+        let mut sorted_triangle = triangle.clone();
+        sorted_triangle.sort();
+        if (sorted_triangle[0] + sorted_triangle[1]) > sorted_triangle[2] {
+            num_possible += 1;
         }
     }
-    num_impossible
+    num_possible
 }
 
 pub fn run() {
     let puzzle_input = std::fs::read_to_string("inputs/day3.txt").unwrap();
-    println!("Part 1 answer: {}", part1(&puzzle_input));
+    let triangles = puzzle_input
+        .lines()
+        .map(|line| line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap()).collect::<Vec<i32>>())
+        .collect::<Vec<_>>();
+
+    let mut transposed_triangles = Vec::new();
+    for i in (0..triangles.len()).step_by(3) {
+        for j in 0..3 {
+            transposed_triangles.push(vec![triangles[i+0][j], triangles[i+1][j], triangles[i+2][j]]);
+        }
+    }
+
+    println!("Part 1 answer: {}", count_possible(&triangles));
+    println!("Part 2 answer: {}", count_possible(&transposed_triangles));
 }
