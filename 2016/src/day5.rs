00dcfe08f11c21183a341249fced9a52f837cd33
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-23 18:30:41 -0700

    2016 Day 5 part 2

--- 2016/src/day5.rs
+++ 2016/src/day5.rs
@@ -1,16 +1,30 @@
 pub fn run() {
     let puzzle_input = "ffykfhsq";
     let mut n : u64 = 0;
-    let mut password = String::new();
+    let mut part1_password = String::new();
+    let mut part2_bytes = vec![b'_'; 8];
 
-    while password.len() < 8 {
-        let hex_md5sum = format!("{:x}", md5::compute(format!("{}{}", puzzle_input, n).as_bytes()));
-        if hex_md5sum.starts_with("00000") {
-            println!("Found hash: {} (n = {})", hex_md5sum, n);
-            password.push(hex_md5sum.chars().nth(5).unwrap());
+    loop {
+        let digest = md5::compute(format!("{}{}", puzzle_input, n).as_bytes());
+        if digest[0] == 0 && digest[1] == 0 && (digest[2] >> 4) == 0 {
+            let hex_md5sum = format!("{:x}", digest);
+
+            if part1_password.len() < 8 {
+                part1_password.push(hex_md5sum.chars().nth(5).unwrap());
+            }
+
+            let position = digest[2] & 0xF;
+            if position < 8 && part2_bytes[position as usize] == b'_' {
+                part2_bytes[position as usize] = (hex_md5sum.chars().nth(6).unwrap() as u8);
+            }
+
+            println!("Found hash: {} (n = {:10}) part1: {:8} part2: {}", hex_md5sum, n, part1_password, String::from_utf8(part2_bytes.clone()).unwrap());
+
+            if part1_password.len() == 8 && !part2_bytes.contains(&b'_') {
+                // Done with both passwords
+                break;
+            }
         }
         n += 1;
     }
-
-    println!("Part 1 answer: {}", password);
 }
