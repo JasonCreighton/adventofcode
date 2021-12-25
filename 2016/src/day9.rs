80582a6eff67edefdcc88c8ee6b6715c52d75c80
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-25 07:42:05 -0700

    2016 Day 9 part 2

--- 2016/src/day9.rs
+++ 2016/src/day9.rs
@@ -16,17 +16,23 @@ fn read_number(input: &mut &[u8]) -> usize {
     n
 }
 
+fn read_marker(ptr: &mut &[u8]) -> (usize, usize) {
+    expect(ptr, b'(');
+    let length = read_number(ptr);
+    expect(ptr, b'x');
+    let times = read_number(ptr);
+    expect(ptr, b')');
+
+    (length, times)
+}
+
 fn decompress(compressed: &[u8]) -> Vec<u8> {
     let mut out = Vec::new();
     let mut ptr = compressed;
 
     while ptr.len() > 0 {
         if ptr[0] == b'(' {
-            expect(&mut ptr, b'(');
-            let length = read_number(&mut ptr);
-            expect(&mut ptr, b'x');
-            let times = read_number(&mut ptr);
-            expect(&mut ptr, b')');
+            let (length, times) = read_marker(&mut ptr);
             for _ in 0..times {
                 out.extend_from_slice(&ptr[0..length]);
             }
@@ -40,6 +46,26 @@ fn decompress(compressed: &[u8]) -> Vec<u8> {
     out
 }
 
+// Note that this is the part 2 algorithm, and is not expected to always give
+// the same length that decompress() gives
+fn decompressed_length(compressed: &[u8]) -> usize {
+    let mut ptr = compressed;
+    let mut prefix_length: usize = 0;
+
+    while ptr.len() > 0 {
+        if ptr[0] == b'(' {
+            let (length, times) = read_marker(&mut ptr);
+            return prefix_length + (decompressed_length(&ptr[0..length]) * times) + decompressed_length(&ptr[length..]);
+        } else {
+            prefix_length += 1;
+            ptr = &ptr[1..];
+        }
+    }
+
+    // If we get here, no marker found
+    prefix_length
+}
+
 #[test]
 fn test_decompress() {
     assert_eq!(decompress(b"ADVENT"), b"ADVENT");
@@ -50,11 +76,20 @@ fn test_decompress() {
     assert_eq!(decompress(b"X(8x2)(3x3)ABCY"), b"X(3x3)ABC(3x3)ABCY");
 }
 
-pub fn run() {
-    let puzzle_input = std::fs::read_to_string("inputs/day9.txt").unwrap();
-    let bytes = puzzle_input.as_bytes();
+#[test]
+fn test_decompressed_length() {
+    assert_eq!(decompressed_length(b"(3x3)XYZ"), 9);
+    assert_eq!(decompressed_length(b"X(8x2)(3x3)ABCY"), 20);
+    assert_eq!(decompressed_length(b"(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920);
+    assert_eq!(decompressed_length(b"(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"), 445);
+}
 
-    let part1_answer = decompress(bytes).len();
+pub fn run() {
+    let puzzle_input = std::fs::read("inputs/day9.txt").unwrap();
+
+    let part1_answer = decompress(&puzzle_input).len();
+    let part2_answer = decompressed_length(&puzzle_input);
 
     println!("Part 1 answer: {}", part1_answer);
+    println!("Part 2 answer: {}", part2_answer);
 }
