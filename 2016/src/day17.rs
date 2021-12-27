1acef9343d62201a7b38beb53c5888243c17330b
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-27 14:57:12 -0700

    2016 Day 17 part 2

--- 2016/src/day17.rs
+++ 2016/src/day17.rs
@@ -23,36 +23,47 @@ fn legal_moves(position: (i32, i32), passcode: &[u8], path: &[u8]) -> Vec<(u8, (
     moves
 }
 
-fn shortest_path(passcode: &[u8]) -> Vec<u8> {
+fn explore_paths(passcode: &[u8]) -> (Vec<u8>, usize) {
     let mut worklist: VecDeque<((i32, i32), Vec<u8>)> = VecDeque::new();
+    let mut longest_path_length = 0;
+    let mut shortest_path: Option<Vec<u8>> = None;
     worklist.push_back(((0, 0), Vec::new()));
 
     while let Some((position, path)) = worklist.pop_front() {
         // Did we find the goal?
         if position == (3, 3) {
-            return path;
-        }
+            longest_path_length = longest_path_length.max(path.len());
 
-        // Add any successor positions to the worklist
-        for (dir, new_position) in legal_moves(position, passcode, path.as_slice()) {
-            let mut new_path = path.clone();
-            new_path.push(dir);
-            worklist.push_back((new_position, new_path));
+            if shortest_path == None {
+                shortest_path = Some(path.clone());
+            }
+        } else {
+            // Add any successor positions to the worklist
+            for (dir, new_position) in legal_moves(position, passcode, path.as_slice()) {
+                let mut new_path = path.clone();
+                new_path.push(dir);
+                worklist.push_back((new_position, new_path));
+            }
         }
     }
 
-    panic!("Should have found goal in worklist loop");
+    match shortest_path {
+        Some(path) => (path, longest_path_length),
+        _ => panic!("Should have found goal in worklist loop"),
+    }
 }
 
 #[test]
 fn test_example() {
-    assert_eq!(shortest_path(b"ihgpwlah").as_slice(), b"DDRRRD");
-    assert_eq!(shortest_path(b"kglvqrro").as_slice(), b"DDUDRLRRUDRD");
-    assert_eq!(shortest_path(b"ulqzkmiv").as_slice(), b"DRURDRUDDLLDLUURRDULRLDUUDDDRR");
+    assert_eq!(explore_paths(b"ihgpwlah"), (b"DDRRRD".to_vec(), 370));
+    assert_eq!(explore_paths(b"kglvqrro"), (b"DDUDRLRRUDRD".to_vec(), 492));
+    assert_eq!(explore_paths(b"ulqzkmiv"), (b"DRURDRUDDLLDLUURRDULRLDUUDDDRR".to_vec(), 830));
 }
 
 pub fn run() {
     const PUZZLE_INPUT: &[u8] = b"pslxynzg";
+    let (shortest_path, longest_path_length) = explore_paths(PUZZLE_INPUT);
 
-    println!("Part 1 answer: {}", String::from_utf8(shortest_path(PUZZLE_INPUT)).unwrap());
+    println!("Part 1 answer: {}", String::from_utf8(shortest_path).unwrap());
+    println!("Part 2 answer: {}", longest_path_length);
 }
