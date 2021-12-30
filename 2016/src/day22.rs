3e0e12236620ddef667b1e5897acc441e6983ff1
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2021-12-30 07:36:24 -0700

    2016 Day 22 part 2

--- 2016/src/day22.rs
+++ 2016/src/day22.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 use regex::Regex;
 
 fn parse_nodes(puzzle_input: &str) -> Vec<(i32, i32, i32, i32, i32)> {
@@ -32,10 +34,69 @@ fn count_viable_pairs(nodes: &[(i32, i32, i32, i32, i32)]) -> i32 {
     viable_pairs
 }
 
+struct Map {
+    width: i8,
+    height: i8,
+    walls: HashSet<(i8, i8)>,
+    starting_empty: (i8, i8),
+}
+
+fn extract_map(nodes: &[(i32, i32, i32, i32, i32)]) -> Map {
+    let mut walls: HashSet<(i8, i8)> = HashSet::new();
+    let mut empty: Option<(i8, i8)> = None;
+    let mut width: i8 = 0;
+    let mut height: i8 = 0;
+
+    for &(x, y, _, used, _) in nodes {
+        width = width.max((x + 1) as i8);
+        height = height.max((y + 1) as i8);
+        if used == 0 {
+            empty = Some((x as i8, y as i8));
+        }
+        if used > 250 {
+            walls.insert((x as i8, y as i8));
+        }
+    }
+
+    Map { width, height, walls, starting_empty: empty.unwrap(), }
+}
+
+fn fewest_steps(map: &Map) -> u32 {
+    let starting_data = (map.width - 1, 0);
+    let mut worklist: VecDeque<(((i8, i8), (i8, i8)), u32)> = VecDeque::new();
+    let mut visited: HashSet<((i8, i8), (i8, i8))> = HashSet::new();
+    worklist.push_back(((map.starting_empty, starting_data), 0));
+
+    while let Some(((empty, data), depth)) = worklist.pop_front() {
+        // Have we been here before?
+        if visited.contains(&(empty, data)) { continue; }
+        visited.insert((empty, data));
+
+        // Did we find the goal?
+        if data == (0, 0) {
+            return depth;
+        }
+
+        // Add possible "moves" of empty square to worklist
+        for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+            let new_empty = (empty.0 + dx, empty.1 + dy);
+            if new_empty.0 < 0 || new_empty.0 >= map.width || new_empty.1 < 0 || new_empty.1 >= map.height { continue; }
+            if map.walls.contains(&new_empty) { continue; }
+
+            let new_data = if new_empty == data { empty } else { data };
+            worklist.push_back(((new_empty, new_data), depth + 1));
+        }
+    }
+
+    panic!("Did not find path to goal!");
+}
+
 pub fn run() {
     let puzzle_input = std::fs::read_to_string("inputs/day22.txt").unwrap();
     let nodes = parse_nodes(&puzzle_input);
     let viable_pairs = count_viable_pairs(&nodes);
+    let map = extract_map(&nodes);
 
     println!("Part 1 answer: {}", viable_pairs);
+    println!("Part 2 answer: {}", fewest_steps(&map));
 }
