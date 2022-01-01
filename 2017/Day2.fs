7289402f5eac30e3248d9e3b698e38056f641a11
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2022-01-01 09:07:53 -0700

    2017 Day 2

---
+++ 2017/Day2.fs
@@ -0,0 +1,37 @@
+﻿module Day2
+
+open Xunit
+
+let parseInput (str : string) =
+    str.Split(System.Environment.NewLine.ToCharArray(), System.StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun s -> Array.map int (s.Split(' ', '\t')))
+
+let part1 = Array.sumBy (fun row -> (Array.max row) - (Array.min row))
+let part2 (ary : int[][]) =
+    Array.sumBy (fun row ->
+        Array.allPairs row row
+        |> Array.filter (fun (a, b) -> a <> b && a % b = 0)
+        |> Array.exactlyOne
+        |> (fun (a, b) -> a / b)
+    ) ary
+
+[<Fact>]
+let testExamples () =
+    let exampleInput1 = "\
+        5 1 9 5\n\
+        7 5 3\n\
+        2 4 6 8\n\
+    "
+    let exampleInput2 = "\
+        5 9 2 8\n\
+        9 4 7 3\n\
+        3 8 6 5\n\
+    "
+    Assert.Equal(parseInput exampleInput1 |> part1, 18)
+    Assert.Equal(parseInput exampleInput2 |> part2, 9)
+
+[<Fact>]
+let testPuzzleInput () =
+    let table = System.IO.File.ReadAllText("../../../inputs/day2.txt").Trim() |> parseInput
+    Assert.Equal(part1 table, 45351)
+    Assert.Equal(part2 table, 275)
