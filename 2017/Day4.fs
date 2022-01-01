31bcfd2f3746b6cc8e2e8cbc1be355a7e37da04a
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2022-01-01 11:09:50 -0700

    2017 Day 4

---
+++ 2017/Day4.fs
@@ -0,0 +1,26 @@
+﻿module Day4
+
+open Xunit
+
+let noDuplicates keyFunc (passphrase : string) =
+    passphrase.Split(' ')
+    |> Seq.countBy keyFunc
+    |> Seq.exists (fun (_, count) -> count <> 1)
+    |> not
+
+let noDuplicateWords = noDuplicates id
+let noAnagrams = noDuplicates (fun s -> Seq.toArray s |> Array.sort)
+
+[<Fact>]
+let testExamples () =
+    Assert.True(noDuplicateWords "aa bb cc dd ee")
+    Assert.False(noDuplicateWords "aa bb cc dd aa")
+    Assert.True(noDuplicateWords "aa bb cc dd aaa")
+
+[<Fact>]
+let testPuzzleInput () =
+    let passphrases = System.IO.File.ReadLines("../../../inputs/day4.txt") |> Seq.toArray
+    let part1Answer = Seq.filter noDuplicateWords passphrases |> Seq.length
+    let part2Answer = Seq.filter noAnagrams passphrases |> Seq.length
+    Assert.Equal(451, part1Answer)
+    Assert.Equal(223, part2Answer)
