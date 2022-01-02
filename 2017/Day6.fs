00bf3f7e69a6a8d7abc9386120da201090302875
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2022-01-02 06:45:52 -0700

    2017 Day 6 part 2

--- 2017/Day6.fs
+++ 2017/Day6.fs
@@ -13,26 +13,28 @@ let reallocate (ary : int[]) =
         ary[j] <- ary[j] + 1
 
 let numSteps initial =
-    let mutable seen = Set.empty
+    let mutable seen = Map.empty
     let mutable ary = initial
     let mutable finished = false
 
     while not finished do
         let asList = Array.toList ary
-        if not (Set.contains asList seen) then            
-            seen <- Set.add asList seen
+        if not (Map.containsKey asList seen) then            
+            seen <- Map.add asList seen.Count seen
             reallocate ary
         else
             finished <- true
 
-    seen.Count
+    (seen.Count, seen.Count - (Map.find (Array.toList ary) seen))
 
 [<Fact>]
 let testExamples () =
     let initial = [|0; 2; 7; 0;|]
-    Assert.Equal(5, numSteps initial)
+    Assert.Equal((5, 4), numSteps initial)
 
 [<Fact>]
 let testPuzzleInput () =
     let initial = Array.copy puzzleInput
-    Assert.Equal(12841, numSteps initial)
+    let part1Answer, part2Answer = numSteps initial
+    Assert.Equal(12841, part1Answer)
+    Assert.Equal(8038, part2Answer)
