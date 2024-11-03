33064b4d95a405efe016b8049c84714f8dcc35a4
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2024-11-03 14:44:50 -0700

    2017 Day 14 part 2

--- 2017/Day14.fs
+++ 2017/Day14.fs
@@ -3,19 +3,53 @@
 open Xunit
 open System.Numerics
 
-let squaresUsed (seed : string) =    
-    [|0..127|]
-    |> Array.sumBy (
-        sprintf "%s-%d" seed
-        >> Day10.binaryKnotHash
-        >> Array.sumBy (uint32 >> BitOperations.PopCount)
-    )
+let usedGrid seed =
+    let grid = Array2D.create 128 128 false
+    for y in 0 .. 127 do
+        let row = sprintf "%s-%d" seed y |> Day10.binaryKnotHash
+        for byteIndex in 0 .. 15 do
+            for bitIndex in 0 .. 7 do
+                let x = (byteIndex * 8) + bitIndex
+                grid[x, y] <- ((row[byteIndex] >>> (7-bitIndex)) &&& 1uy) <> 0uy
+
+    grid
+
+let squaresUsed (seed : string) : int =
+    usedGrid seed
+    |> Seq.cast<bool>
+    |> Seq.sumBy (fun used -> if used then 1 else 0)
+
+let rec fillContiguous (used : bool[,]) (seen : bool[,]) (x : int) (y : int) =
+    if x >= used.GetLowerBound(0) && x <= used.GetUpperBound(0) && y >= used.GetLowerBound(1) && y <= used.GetUpperBound(1) then
+        if used[x, y] && not seen[x, y] then
+            seen[x, y] <- true
+            fillContiguous used seen (x + 0) (y + 1)
+            fillContiguous used seen (x + 0) (y - 1)
+            fillContiguous used seen (x + 1) (y + 0)
+            fillContiguous used seen (x - 1) (y + 0)
+        
+
+let regionsPresent (seed : string) : int =
+    let mutable numRegions = 0
+    let used = usedGrid seed
+    let seen = Array2D.create 128 128 false
+
+    for y in 0 .. 127 do
+        for x in 0 .. 127 do
+            if used[x, y] && not seen[x, y] then
+                fillContiguous used seen x y
+                numRegions <- numRegions + 1
+
+    numRegions
 
 [<Fact>]
 let testExamples () =
-    Assert.Equal(8108, squaresUsed "flqrgnkx")
+    let exampleInput = "flqrgnkx"
+    Assert.Equal(8108, squaresUsed exampleInput)
+    Assert.Equal(1242, regionsPresent exampleInput)
 
 [<Fact>]
 let testPuzzleInput () =
     let puzzleInput = "jzgqcdpd"
     Assert.Equal(8074, squaresUsed puzzleInput)
+    Assert.Equal(1212, regionsPresent puzzleInput)
