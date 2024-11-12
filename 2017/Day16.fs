982b11c667bc6bbe3c98bdbe9b8300d620ae1474
Jason Creighton <34358430+JasonCreighton@users.noreply.github.com>
2024-11-11 18:26:21 -0700

    2017 Day 16 part 2

--- 2017/Day16.fs
+++ 2017/Day16.fs
@@ -20,35 +20,55 @@ let partner (dancerA : char) (dancerB : char) (dancers : array<char>) : array<ch
     let idxB = Array.findIndex (fun d -> d = dancerB) dancers
     exchange idxA idxB dancers
 
-
-let textCommand (dancers: array<char>) (cmd : string) : array<char> =
+let parseCommand (cmd : string) : (array<char> -> array<char>) =
     match cmd[0] with
-    | 's' -> spin (int cmd[1..]) dancers
+    | 's' -> spin (int cmd[1..])
     | 'x' ->
         let [|a; b|] = Array.map int (cmd[1..].Split("/"))
-        exchange a b dancers
+        exchange a b
     | 'p' ->
         let [|dancerA; dancerB|] = Array.map (fun (s : string) -> s[0]) (cmd[1..].Split("/"))
-        partner dancerA dancerB dancers
+        partner dancerA dancerB
+
+let dance (commands : array<array<char> -> array<char>>) (state : array<char>) : array<char> =
+    Array.fold (|>) state commands
+
+let findCycle commands initial =
+    // Infinite list of the results of all dances
+    let danceResults = Seq.unfold (fun s -> Some (s, dance commands s)) (dance commands initial)
+
+    // We hope that we return to the initial state at some point, otherwise it would be
+    // too hard to calculate a billion dances.
+    let cycle = Seq.takeWhile (fun s -> s <> initial) danceResults
+
+    // Stick the initial state at the beginning to make the indexing work out nicer.
+    Array.ofSeq (Seq.append (Seq.singleton initial) cycle)
+
+let part2 commands initial =
+    let cycle = findCycle commands initial
+    cycle[1000000000 % Array.length cycle]
 
 [<Fact>]
 let testExamples () =
     let state0 = [|'a' .. 'e'|]
 
-    let state1 = textCommand state0 "s1" 
+    let state1 = parseCommand "s1" state0
     Assert.Equal<char>([|'e'; 'a'; 'b'; 'c'; 'd'|], state1)
 
-    let state2 = textCommand state1 "x3/4" 
+    let state2 = parseCommand "x3/4" state1
     Assert.Equal<char>([|'e'; 'a'; 'b'; 'd'; 'c'|], state2)
 
-    let state3 = textCommand state2 "pe/b" 
+    let state3 = parseCommand "pe/b" state2
     Assert.Equal<char>([|'b'; 'a'; 'e'; 'd'; 'c'|], state3)
 
 [<Fact>]
 let testPuzzleInput () =
     let puzzleInput = System.IO.File.ReadAllText("../../../inputs/day16.txt").Trim()
-    let commands = puzzleInput.Split(",")
+    let commands = puzzleInput.Split(",") |> Array.map parseCommand
     let initial = [|'a' .. 'p'|];    
-    let final = Array.fold textCommand initial commands
+    let final = Array.fold (|>) initial commands
     let part1_answer = System.String final
     Assert.Equal("kpbodeajhlicngmf", part1_answer)
+
+    let part2_answer = part2 commands initial |> System.String
+    Assert.Equal("ahgpjdkcbfmneloi", part2_answer)
